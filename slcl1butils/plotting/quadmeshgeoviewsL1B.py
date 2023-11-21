ede7c9fa86a94d2b9d6ac0797f5ad1d8d9372f32
grouazel <antoine.grouazel@ifremer.fr>
2023-11-21 12:04:33 +0100

    improve slcl1butils/plotting/quadmeshgeoviewsL1B.py to be able to display cwaves

--- slcl1butils/plotting/quadmeshgeoviewsL1B.py
+++ slcl1butils/plotting/quadmeshgeoviewsL1B.py
@@ -1,3 +1,5 @@
+import pdb
+
 import xarray as xr
 import numpy as np
 import holoviews as hv
@@ -14,8 +16,7 @@ def doQuadMeshL1BorL1C_onefile(ff,bursttype='intraburst',variable='sigma0',clim=
     qmeshes = gv.Dataset(ff, kdims=['longitude', 'latitude']).to(gv.QuadMesh)
     # clipping = {"min": "red", "max": "green", "NaN": "gray"}
     clipping = {"NaN": "blue"} # see https://holoviews.org/user_guide/Styling_Plots.html
-    # print('go clipping')
-    fig = qmeshes.opts(width=900, height=600, colorbar=True, cmap=cmap,clipping_colors=clipping) #clim=clim
+    fig = qmeshes.opts(width=1200, height=900, colorbar=True, cmap=cmap,clipping_colors=clipping,tools=['hover']) #clim=clim
     return fig
 
 def open_of_use(ff,bursttype):
@@ -28,23 +29,44 @@ def open_of_use(ff,bursttype):
 
     """
     if isinstance(ff,str):
-        ds = xr.open_dataset(ff, group=bursttype)
+        ds = xr.open_dataset(ff, group=bursttype,engine='h5netcdf')
     else:
         ds = ff
     return ds
 def prepare_L1BC(ff,bursttype,variable,**kwargs):
+    """
+
+    Args:
+        ff: str or xr.Dataset
+        bursttype: str
+        variable: str
+    Kwargs:
+        kwargs (dict): optional keyword arguments : k_gp (int) phi_hf (int) lambda_range_max_macs(int) are valid entries
+    Returns:
+
+    """
     ds = open_of_use(ff, bursttype)
     # ds.load()
     # ds = datatree.open_datatree(ff)[bursttype].to_dataset() # it doesnt change anything to segmentation fault...
-    if variable in ['cwave']:
-        sub = ds[variable].isel({'k_gp':kwargs['k_gp'],'phi_hf':kwargs['phi_hf']}).stack({'y': ['burst', 'tile_line']})
+    if variable in ['cwave_params']:
+        sub = ds[variable].isel({'2tau':0,'k_gp':kwargs.get('k_gp'),'phi_hf':kwargs.get('phi_hf')}).stack({'y': ['burst', 'tile_line']})
+    elif variable in ['macs_Im','macs_Re']:
+        sub = ds[variable].isel({'lambda_range_max_macs':kwargs.get('lambda_range_max_macs')}).stack({'y': ['burst', 'tile_line']})
     else:
         sub = ds[variable].stack({'y': ['burst', 'tile_line']})
     if np.isnan(sub).any():
-        logging.info('there are %s NaN in the variable stacked : %s',np.isnan(sub.data).sum(),variable)
+        logging.debug('there are %s NaN in the variable stacked : %s',np.isnan(sub.data).sum(),variable)
     sub = sub.rename({'tile_sample': 'x'})
     sub = sub.assign_coords({'y': np.arange(sub.y.size)})
     sub = sub.assign_coords({'x': np.arange(sub.x.size)})
+    if (sub==0).any() and variable=='sigma0':
+        pdb.set_trace()
+    else:
+        pass
+        # print('debug not zeros found -> continue')
+    if np.isnan(sub).any() and variable=='sigma0' and (ds['land_flag'].data==False).all():
+        print('Nan alerte')
+        pdb.set_trace()
     return sub
 
 def doQuadMeshL1BorL1C_manyfiles(files,bursttype='intraburst',variable='sigma0',clim=(0,0.2),title='',cmap='Greys_r'):
@@ -54,14 +76,16 @@ def doQuadMeshL1BorL1C_manyfiles(files,bursttype='intraburst',variable='sigma0',
         all_quads.append(oneQuad)
     tiles = gv.tile_sources.EsriImagery
     fig = tiles * hv.Overlay(all_quads)
-    fig.opts(title=title)
+    fig.opts(title=title,tools=['hover'])
     return fig
 
-def doQuadMeshL1BorL1C_manyfiles_opt(files,variables,bursttype='intraburst',clim=(0,0.2),title='',cmap='Greys_r',outputdir=None,**kwargs):
+def doQuadMeshL1BorL1C_manyfiles_opt(files,pol,variables,bursttype='intraburst',clim=(0,0.2),title='',cmap='Greys_r',
+                                     outputdir=None,outbasename=None,**kwargs):
     """
 
     Args:
         files: list of fulll path str
+        pol : str vv vh hv hh
         variables: list of string
         bursttype: str intraburts or interburst
         clim: tuple eg (0.2,4.5)
@@ -74,6 +98,7 @@ def doQuadMeshL1BorL1C_manyfiles_opt(files,variables,bursttype='intraburst',clim
     Returns:
 
     """
+    fig = None
     allds = {}
     for ff in files:
         allds[ff] = open_of_use(ff,bursttype)
@@ -89,7 +114,7 @@ def doQuadMeshL1BorL1C_manyfiles_opt(files,variables,bursttype='intraburst',clim
         logging.info('variable = %s',var)
         for ff in files:
             ds = allds[ff]
-            sub = prepare_L1BC(ds, bursttype, var,kwargs=kwargs)
+            sub = prepare_L1BC(ds, bursttype, var,**kwargs)
             subs['%s_%s'%(var,ff)] = sub
             subdsx.append(sub)
         fig = doQuadMeshL1BorL1C_manyfiles(
@@ -104,7 +129,10 @@ def doQuadMeshL1BorL1C_manyfiles_opt(files,variables,bursttype='intraburst',clim
             renderer = hv.renderer("bokeh")
             #
             # Using renderer save
-            outf = os.path.join(outputdir, "map_%s" % var)
+            if outbasename is None:
+                outf = os.path.join(outputdir, "map_%s_%s" % (pol,var))
+            else:
+                outf = os.path.join(outputdir,outbasename)
             renderer.save(fig, outf)
             # save(fig, outf)
             logging.info('output file: %s',outf+'.html')
