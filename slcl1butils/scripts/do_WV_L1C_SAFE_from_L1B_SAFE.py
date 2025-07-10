77c08042a7434daf8817503d39971ba19101b80c
agrouaze <antoine.grouazel@ifremer.fr>
2025-07-10 13:56:31 +0000

    keep the k_rg and k_az as variable

--- slcl1butils/scripts/do_WV_L1C_SAFE_from_L1B_SAFE.py
+++ slcl1butils/scripts/do_WV_L1C_SAFE_from_L1B_SAFE.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import pdb
 import time
 import warnings
 from collections import defaultdict
@@ -21,7 +22,7 @@ from slcl1butils.coloc.coloc_WV_WW3spectra import (
     resampleWW3spectra_on_SAR_cartesian_grid,
 )
 from slcl1butils.compute.compute_from_l1b import compute_xs_from_l1b_wv,get_start_date_from_attrs
-from slcl1butils.get_config import get_conf
+from slcl1butils.get_config import get_conf,get_product_id_parameters
 from slcl1butils.raster_readers import (
     ecmwf_0100_1h,
     resource_strftime,
@@ -38,6 +39,7 @@ def do_L1C_SAFE_from_L1B_SAFE(
     full_safe_file,
     productid,
     outputdir,
+    product_configuration,
     colocat=True,
     time_separation="2tau",
     overwrite=False,
@@ -71,11 +73,15 @@ def do_L1C_SAFE_from_L1B_SAFE(
     # ancillary_ww3["step"] = 3
     # ancillary_ww3["name"] = "ww3_global_yearly_3h"
     # "ww3hindcast_field": conf["auxilliary_dataset"]["ww3_global_cciseastate"],
-    ancillary_list = {
-        "ecmwf_0100_1h": conf["auxilliary_dataset"]["ecmwf_0100_1h"],
-        # "ww3hindcast_field": conf["auxilliary_dataset"]["ww3hindcast_field"],
-        "ww3hindcast_field": conf["auxilliary_dataset"]["ww3_global_cciseastate"],
-    }
+    # ancillary_list = {
+    #     "ecmwf_0100_1h": conf["auxilliary_dataset"]["ecmwf_0100_1h"],
+    #     # "ww3hindcast_field": conf["auxilliary_dataset"]["ww3hindcast_field"],
+    #     "ww3hindcast_field": conf["auxilliary_dataset"]["ww3_global_cciseastate"],
+    # }
+    ancillary_list = {}
+    for iix,uu in enumerate(product_configuration['ancillary_raster_dataset']):
+        # ancillary_list[uu] = product_configuration['ancillary_raster_dataset'][iix]
+        ancillary_list[uu] = conf["auxilliary_dataset"][uu]
     # ancillary_list = [ancillary_ecmwf]#,ancillary_ww3]
     # ancillary_list = [ancillary_ecmwf, ancillary_ww3]
     logging.info("ancillary data: %s", ancillary_list)
@@ -120,6 +126,7 @@ def do_L1C_SAFE_from_L1B_SAFE(
             ancillaries=ancillary_list,
             colocat=colocat,
             time_separation=time_separation,
+            product_configuration=product_configuration
         )
         # if ancillary_product_found:
         #     cpt_ancillary_products_found += 1
@@ -143,7 +150,7 @@ def do_L1C_SAFE_from_L1B_SAFE(
 
 
 def enrich_onesubswath_l1b(
-    l1b_fullpath, ancillaries=None, colocat=True, time_separation="2tau"
+    l1b_fullpath,product_configuration, ancillaries=None, colocat=True, time_separation="2tau",
 ):
     """
 
@@ -170,7 +177,7 @@ def enrich_onesubswath_l1b(
     #
     # Intraburst at 2tau x-spectra
     xs_intra_groups, ds_intra = compute_xs_from_l1b_wv(
-        l1b_fullpath, time_separation=time_separation
+        l1b_fullpath, time_separation=time_separation,crop_limits=product_configuration['crop_xspectra']
     )
 
     # ====================
@@ -190,7 +197,7 @@ def enrich_onesubswath_l1b(
             ancillaries_flag_added[official_name_ancillary] = flag_ancillary_field_added
 
     # this part is commented temporarily to test only the association with raster fields alone
-    if "WV" in l1b_fullpath:
+    if "WV" == product_configuration['mode'] and product_configuration['add_ww3spectra'] is True:
         logging.info("ancillary WW3 spectra")
         dims_to_expand = ["tile_sample", "tile_line"]
         imagettestiles_sizes = {d: k for d, k in ds_intra["longitude"].sizes.items()}
@@ -200,6 +207,7 @@ def enrich_onesubswath_l1b(
             range(len(all_cases))
         ):  # loop over tile_sample, tile_line and time
             i = all_cases[ix]
+
             one_tile = ds_intra[i]
             (
                 one_tile,
@@ -208,26 +216,46 @@ def enrich_onesubswath_l1b(
             ) = resampleWW3spectra_on_SAR_cartesian_grid(dsar=one_tile)
             if flag_ww3spectra_found:
                 ancillaries_flag_added["ww3spectra"] = flag_ww3spectra_added
-            if (
-                ix == 0
-            ):  # approximation all the WV of a given incidence (WV1 or WV2) can use the same k_az and k_rg
-                all_coord_to_drop = [
-                    uu
-                    for uu in one_tile["k_az"].coords
-                    if uu not in ["k_az", "k_rg", "ky", "kx"]
-                ]
-                k_az_ref = one_tile["k_az"].drop(all_coord_to_drop)
-                k_rg_ref = one_tile["k_rg"].drop(all_coord_to_drop)
-            else:
-                one_tile = one_tile.assign_coords({"k_az": k_az_ref})
-                one_tile = one_tile.assign_coords({"k_rg": k_rg_ref})
+            # part to replace individual k_rg and k_az vectors by a common one
+            # if (
+            #     ix == 0
+            # ):  # approximation all the WV of a given incidence (WV1 or WV2) can use the same k_az and k_rg
+            #     all_coord_to_drop = [
+            #         uu
+            #         for uu in one_tile["k_az"].coords
+            #         if uu not in ["k_az", "k_rg", "ky", "kx"]
+            #     ]
+            #     k_az_ref = one_tile["k_az"].drop(all_coord_to_drop)
+            #     k_rg_ref = one_tile["k_rg"].drop(all_coord_to_drop)
+            # else:
+            #     one_tile = one_tile.assign_coords({"k_az": k_az_ref})
+            #     one_tile = one_tile.assign_coords({"k_rg": k_rg_ref})
             one_tile["time"] = xr.DataArray([one_tile["time"].values], dims="time")
             one_tile["longitude"] = xr.DataArray([one_tile["longitude"]], dims="time")
             one_tile["latitude"] = xr.DataArray([one_tile["latitude"]], dims="time")
             #one_tile["sensing_time"] = xr.DataArray(
             #    [one_tile["sensing_time"].values], dims="time"
             #)
-            out.append(one_tile)
+            # replace the dependency to k_rg and k_az to freq_line and freq_sample
+            # --- 2. Define the new coordinates you want ---
+            # The new coordinates are simple integer ranges
+            freq_line_coords = np.arange(one_tile.dims['k_az'])
+            freq_sample_coords = np.arange(one_tile.dims['k_rg'])
+
+            # --- 3. Apply the transformation in a single, chained command ---
+            # Let's name your original dataset `ds_original` for clarity
+
+            ds_new = (
+                one_tile
+                .reset_index(['k_az', 'k_rg'], drop=False)
+                .rename({'k_az': 'freq_line', 'k_rg': 'freq_sample'})
+                .assign_coords({
+                    'freq_line': freq_line_coords,
+                    'freq_sample': freq_sample_coords
+                })
+            )
+            ds_new = ds_new.rename({'kx':'k_rg','ky':'k_az'})
+            out.append(ds_new)
         # ds_intra = xr.combine_by_coords([x.expand_dims(dims_to_expand) for x in out], combine_attrs='drop_conflicts') # killed on a 17.5km tile
         ds_intra = xr.concat([x.expand_dims(dims_to_expand) for x in out], dim="time")
     return ds_intra, ancillaries_flag_added
@@ -422,6 +450,12 @@ def main():
         default=conf["l1c_wv_version"],
     )
     parser.add_argument(
+        "--configproducts",
+        help="path of a yaml config file where the different versions of products are defined",
+        required=False,
+        default=os.path.join(os.path.dirname(slcl1butils.__file__),'BXX_description.yml')
+    )
+    parser.add_argument(
         "--dev",
         action="store_true",
         default=False,
@@ -440,6 +474,7 @@ def main():
     t0 = time.time()
     logging.info("product productid to produce: %s", args.productid)
     logging.info("outputdir will be: %s", args.outputdir)
+    confproduct = get_product_id_parameters(args.configproducts,product_id=args.productid)
     (
         cpt_success,
         cpt_already,
@@ -448,6 +483,7 @@ def main():
     ) = do_L1C_SAFE_from_L1B_SAFE(
         args.l1bsafe,
         productid=args.productid,
+        product_configuration=confproduct,
         outputdir=args.outputdir,
         colocat=True,
         time_separation="2tau",
